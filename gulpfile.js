e804a4fbe95c4bf03240a5132ebc575f661955f0
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2016-09-30 21:30:20 -0500

    Update gulp tasks
    - Add sass compilation task and copy-fonts task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
-var webpackConf = require('./webpack.config.js');
-var webpackStream = require('webpack-stream');
+var npm = require('global-npm');
+var sass = require('gulp-sass');
 
 var paths = {
     sass: 'src/**/*.scss',
@@ -8,10 +8,21 @@ var paths = {
     html: 'src/**/*.html'
 };
 
-gulp.task('build', function () {
-    return gulp.src('./src/cards.ts')
-        .pipe(webpackStream(webpackConf))
-        .pipe(gulp.dest('www/dist'));
+gulp.task('build', ['sass'], function (done) {
+    npm.load({}, function () {
+        npm.commands.run(['build'], done);
+    });
+});
+
+gulp.task('sass', ['copy-fonts'], function () {
+    return gulp.src('./src/test.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./www/css'));
+});
+
+gulp.task('copy-fonts', function () {
+    return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,woff2,eof,svg}')
+        .pipe(gulp.dest('./www/fonts'));
 });
 
 gulp.task('build-dist', function () {

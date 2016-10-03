dbc3fe17d11eb740b1914828f21ebaad44f6d2b1
Luis Hernandez <luis.hlopez@outlook.com>
2016-10-03 12:02:09 -0500

    Add validations for the card

--- src/card/card-controller.ts
+++ src/card/card-controller.ts
@@ -1,15 +1,35 @@
-export default class CardController {
+export class CardController {
     public title: string;
     public height: string;
     public hasFooter: boolean;
     public canFlip: boolean;
     public isFlipped: boolean = false;
+    public onSaveSettings: Function;
 
-    constructor(){
+    constructor() {
         this.title = this.title || 'Card Title';
     }
 
     public toggleFlip(): void {
         this.isFlipped = !this.isFlipped;
+        this._checkCardHeightIsValid();
+    }
+
+    public saveSettings(): void {
+        try {
+            this.onSaveSettings();
+        } catch (e) {
+            alert('ERROR: Cards that can flip must define the onSaveSettings property with a valid function!');
+        } finally {
+            this.toggleFlip();
+        }
+    }
+
+    private _checkCardHeightIsValid(): void {
+        if (parseInt(this.height) < 1) {
+            this.height = '1';
+        } else if (parseInt(this.height) > 4) {
+            this.height = '4';
+        }
     }
 }
